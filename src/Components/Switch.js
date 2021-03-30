a26c41104e504983e83fbf1d18a4e219ffc5d3c8
王培 <1390516519@qq.com>
2021-03-30 17:30:57 +0800

    创建： 基础构建

---
+++ src/Components/Switch.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Switch() {
+    return (
+        <h1>Switch</h1>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import Switch from './Components/Switch'
+
+// export { Switch }
+
+// 两种写法
+
+export { default as Switch } from './Components/Switch'
